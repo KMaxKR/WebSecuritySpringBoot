2b2867a61ed115778e1eb9f420dd3f37b37fab1e
Cresciuc Maxim <cresciucmax@gmail.com>
2023-07-08 16:54:07 +0300

    main

---
+++ WebSecuritySpringBoot-master/secur/src/main/java/ks/secur/entity/Developer.java
@@ -0,0 +1,14 @@
+package ks.secur.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
